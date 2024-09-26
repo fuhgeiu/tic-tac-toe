0f588c0805383186a39590a8208ef9923b971b98
emf09 <93343039+fuhgeiu@users.noreply.github.com>
2024-09-26 00:07:45 -0500

    initial

---
+++ tic tac toe xcode/tic tac toe xcode/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  tic tac toe xcode
+//
+//  Created by Alex Carnes on 9/26/24.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
